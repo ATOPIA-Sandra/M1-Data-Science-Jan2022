536a77092b4f2bcd8142d9bea79b1b46e72d4706
Balignissa <balignissa@gmail.com>
2022-04-05 20:57:44 +0000

    ""

---
+++ .history/Untitled_20220405205744.py
@@ -0,0 +1,39 @@
+class Distance:
+    __metric = {
+        "mm": 0.001,
+        "cm": 0.01,
+        "m": 1,
+        "km": 1000,
+        "in": 0.0254,
+        "ft": 0.3048,
+        "yd": 0.9144,
+        "mi": 1609.344,
+    }
+    
+    def __init__(self, value, unit="m"):
+        self.value = value
+        self.unit = unit
+        
+    def conv(self):
+        return self.value*Distance.__metric[self.unit]
+        
+    def__add__(self, other):
+        new_value = self.value + other.value
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
